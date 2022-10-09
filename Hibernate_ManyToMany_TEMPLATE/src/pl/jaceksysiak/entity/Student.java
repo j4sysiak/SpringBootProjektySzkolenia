78b165df1cc8af5b6bef2e39eba40305091f1202
Jacek Sysiak <j4sysiak@gmail.com>
2022-10-09 22:19:42 +0200

    09-10-2022 21:54

---
+++ Hibernate_ManyToMany_TEMPLATE/src/pl/jaceksysiak/entity/Student.java
@@ -0,0 +1,100 @@
+package pl.jaceksysiak.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstname;
+	
+	@Column(name="last_name")
+	private String lasttname;
+	
+	@Column(name="email")
+	private String email;
+	
+	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH,
+			CascadeType.REFRESH })
+	@JoinTable(name="course_student", 
+			   joinColumns = @JoinColumn(name = "student_id"), 
+			   inverseJoinColumns = @JoinColumn(name = "course_id")
+	           )
+	private List<Course> courses;
+	
+	public Student() {}
+
+	public Student(String firstname, String lasttname, String email) {
+		this.firstname = firstname;
+		this.lasttname = lasttname;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLasttname() {
+		return lasttname;
+	}
+
+	public void setLasttname(String lasttname) {
+		this.lasttname = lasttname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstname=" + firstname + ", lasttname=" + lasttname + ", email=" + email + "]";
+	}
+    
+	
+	
+}
