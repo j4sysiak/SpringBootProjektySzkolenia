b2ce5b515d5f3bd003966b7ae38fc1a59f7cbd4d
Jacek Sysiak <j4sysiak@gmail.com>
2022-10-09 22:31:34 +0200

    09-10-2022 21:55

---
+++ HibernateKurs5_Sekcja5_Mapping_Collection_Basic_Value_Type/src/main/java/pl/jaceksysiak/demo/Application_CreateData.java
@@ -0,0 +1,61 @@
+package pl.jaceksysiak.demo;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import pl.jaceksysiak.demo.entity.Address;
+import pl.jaceksysiak.demo.entity.Bank;
+
+
+public class Application_CreateData {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Bank.class)
+								.addAnnotatedClass(Address.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {	
+			// start a transaction
+			session.beginTransaction();
+			
+			//creating Bank
+			Bank tmpBank = new Bank();
+			tmpBank.setName("BRE Bank");
+			tmpBank.setInternational(false);
+			tmpBank.setLastUpdatedDate(new Date());
+			tmpBank.setLastUpdatedBy("Jacek Sysiak");
+			tmpBank.setCreatedDate(new Date());
+			tmpBank.setCreatedBy("Jacek Sysiak");
+			tmpBank.setAddressLine1("hhhhhhhhhhhhhhhhhh");
+			tmpBank.setAddressLine2("rrrrrrrrrrrrrrrrrrrrrr");
+			tmpBank.setCity("New York");
+			tmpBank.setState("NY");
+			tmpBank.setZipCode("78-90-99");
+			tmpBank.getContacts().add("Jacek");
+			tmpBank.getContacts().add("Maria");
+			tmpBank.getContacts().add("Ewa");
+			
+			// save the Bank
+			session.save(tmpBank);
+						
+			// commit transaction
+			session.getTransaction().commit();
+		}
+		finally {
+			
+			// add clean up code
+			session.close();			
+			factory.close();
+		}
+	}
+}
