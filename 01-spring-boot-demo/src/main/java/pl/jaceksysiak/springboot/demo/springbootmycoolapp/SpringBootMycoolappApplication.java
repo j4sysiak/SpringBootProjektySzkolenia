2a56d200944fff06d9942ff3dc584841b351563a
Jacek Sysiak <j4sysiak@gmail.com>
2022-10-09 21:56:06 +0200

    09-10-2022 21:53

---
+++ 01-spring-boot-demo/src/main/java/pl/jaceksysiak/springboot/demo/springbootmycoolapp/SpringBootMycoolappApplication.java
@@ -0,0 +1,13 @@
+package pl.jaceksysiak.springboot.demo.springbootmycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMycoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMycoolappApplication.class, args);
+	}
+
+}
